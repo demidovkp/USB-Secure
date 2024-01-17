d1c8b84708526a190759106a4f17d47230e8e5dc
demidovkp <demidov.kp@ya.ru>
2024-01-17 14:34:35 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication
+{
+    partial class USBSecure
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckUSBConnections = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tbNameDisk = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CheckUSBConnections
+            // 
+            this.CheckUSBConnections.Location = new System.Drawing.Point(12, 12);
+            this.CheckUSBConnections.Name = "CheckUSBConnections";
+            this.CheckUSBConnections.Size = new System.Drawing.Size(146, 23);
+            this.CheckUSBConnections.TabIndex = 0;
+            this.CheckUSBConnections.Text = "Проверка USB событий";
+            this.CheckUSBConnections.UseVisualStyleBackColor = true;
+            this.CheckUSBConnections.Click += new System.EventHandler(this.CheckUSBConnections_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 101);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "4622896404899915";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Отключить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(134, 150);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Запрет всех флешек";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tbNameDisk
+            // 
+            this.tbNameDisk.Location = new System.Drawing.Point(252, 14);
+            this.tbNameDisk.Name = "tbNameDisk";
+            this.tbNameDisk.Size = new System.Drawing.Size(38, 20);
+            this.tbNameDisk.TabIndex = 9;
+            this.tbNameDisk.Text = "G";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // USBSecure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 203);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbNameDisk);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CheckUSBConnections);
+            this.Name = "USBSecure";
+            this.Text = "USB Secure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckUSBConnections;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox tbNameDisk;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
+
+namespace WindowsFormsApplication
+{
+    public partial class USBSecure : Form
+    {
+        public USBSecure()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr SecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile
+        );
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            IntPtr lpInBuffer,
+            uint nInBufferSize,
+            IntPtr lpOutBuffer,
+            uint nOutBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped
+        );
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool DeviceIoControl(
+            IntPtr hDevice,
+            uint dwIoControlCode,
+            byte[] lpInBuffer,
+            uint nInBufferSize,
+            IntPtr lpOutBuffer,
+            uint nOutBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        private IntPtr handle = IntPtr.Zero;
+
+        const uint GENERIC_READ = 0x80000000;
+        const uint GENERIC_WRITE = 0x40000000;
+        const int FILE_SHARE_READ = 0x1;
+        const int FILE_SHARE_WRITE = 0x2;
+        const int FSCTL_LOCK_VOLUME = 0x00090018;
+        const int FSCTL_DISMOUNT_VOLUME = 0x00090020;
+        const int IOCTL_STORAGE_EJECT_MEDIA = 0x2D4808;
+        const int IOCTL_STORAGE_MEDIA_REMOVAL = 0x002D4804;
+
+        /// <summary>
+        /// Constructor for the USBEject class
+        /// </summary>
+        /// <param name="driveLetter">This should be the drive letter. Format: F:/, C:/..</param>
+
+        public IntPtr USBEject(string driveLetter)
+        {
+            string filename = @"\\.\" + driveLetter[0] + ":";
+            return CreateFile(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, IntPtr.Zero, 0x3, 0, IntPtr.Zero);
+        }
+
+        public bool Eject(IntPtr handle)
+        {
+            bool result = false;
+
+            if (LockVolume(handle) && DismountVolume(handle))
+            {
+                PreventRemovalOfVolume(handle, false);
+                result = AutoEjectVolume(handle);
+            }
+            CloseHandle(handle);
+            return result;
+        }
+
+        private bool LockVolume(IntPtr handle)
+        {
+            uint byteReturned;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (DeviceIoControl(handle, FSCTL_LOCK_VOLUME, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero))
+                {
+                    System.Windows.Forms.MessageBox.Show("Lock success!");
+                    return true;
+                }
+                Thread.Sleep(500);
+            }
+            return false;
+        }
+
+        private bool PreventRemovalOfVolume(IntPtr handle, bool prevent)
+        {
+            byte[] buf = new byte[1];
+            uint retVal;
+
+            buf[0] = (prevent) ? (byte)1 : (byte)0;
+            return DeviceIoControl(handle, IOCTL_STORAGE_MEDIA_REMOVAL, buf, 1, IntPtr.Zero, 0, out retVal, IntPtr.Zero);
+        }
+
+        private bool DismountVolume(IntPtr handle)
+        {
+            uint byteReturned;
+            return DeviceIoControl(handle, FSCTL_DISMOUNT_VOLUME, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero);
+        }
+
+        private bool AutoEjectVolume(IntPtr handle)
+        {
+            uint byteReturned;
+            return DeviceIoControl(handle, IOCTL_STORAGE_EJECT_MEDIA, IntPtr.Zero, 0, IntPtr.Zero, 0, out byteReturned, IntPtr.Zero);
+        }
+
+        private bool CloseVolume(IntPtr handle)
+        {
+            return CloseHandle(handle);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------
+
+        #region
+        /// <summary>
+        /// Регистрируем событие USB
+        /// </summary>
+        class USBConnectionsChecker
+        {
+
+            private int devicesCount;
+            private Timer updatingInformationTimer;
+
+            public event EventHandler DeviceConnected;
+            public event EventHandler DeviceDisconnected;
+
+            private int GetDevicesCount()
+            {
+                return ((new ManagementObjectSearcher(@"select * from Win32_USBHub")).Get()).Count;
+            }
+
+            private void UpdatingInformationTimer_Tick(object sender, EventArgs e)
+            {
+                int newDevicesCountValue = GetDevicesCount();
+
+                if (newDevicesCountValue != devicesCount)
+                {
+                    if (newDevicesCountValue > devicesCount)
+                    {
+                        devicesCount = newDevicesCountValue;
+                        DeviceConnected(this, null);
+                    }
+                    else
+                    {
+                        devicesCount = newDevicesCountValue;
+                        DeviceDisconnected(this, null);
+                    }
+                }
+            }
+
+            public USBConnectionsChecker()
+            {
+                devicesCount = GetDevicesCount();
+                updatingInformationTimer = new Timer();
+                updatingInformationTimer.Tick += new EventHandler(this.UpdatingInformationTimer_Tick);
+                updatingInformationTimer.Interval = 1000;
+                updatingInformationTimer.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Событие включение флешки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UsbConnectionsChecker_DeviceConnected(object sender, System.EventArgs e)
+        {
+            // Отключаем все флешки
+            if (checkBox1.Checked)
+            {
+                return;
+            }
+
+            var usbDevices = GetUSBDevices();
+            foreach (var usbDevice in usbDevices)
+            {
+                var sn = usbDevice.PnpDeviceID.ToString().Split('&').First();
+                if (sn == textBox2.Text)
+                {
+                    textBox1.AppendText($"PNP {sn} - SN {usbDevice.SerialNumber} ===== Есть в базе !");
+                    textBox1.AppendText(Environment.NewLine);
+                }
+                else textBox1.AppendText($"PNP {sn} - SN " + usbDevice.SerialNumber);
+                // $"Device ID: {usbDevice.DeviceID}, PNP Device ID: {usbDevice.PnpDeviceID}, Description: {usbDevice.Description}");
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        /// <summary>
+        /// Событие отключение флешки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UsbConnectionsChecker_DeviceDisconnected(object sender, System.EventArgs e)
+        {
+            textBox1.AppendText("Один из USB-девайсов был отключен!"); 
+            textBox1.AppendText(Environment.NewLine);
+        }
+
+        private void CheckUSBConnections_Click(object sender, EventArgs e)
+        {
+            UsbConnectionsChecker_DeviceConnected(sender, e);
+
+            USBConnectionsChecker usbConnectionsChecker = new USBConnectionsChecker();
+            usbConnectionsChecker.DeviceConnected += new EventHandler(UsbConnectionsChecker_DeviceConnected);
+            usbConnectionsChecker.DeviceDisconnected += new EventHandler(UsbConnectionsChecker_DeviceDisconnected);
+        }
+        #endregion
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+            var searcher = new ManagementObjectSearcher("root\\CIMV2",
+                @"SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            ManagementObjectCollection collection = searcher.Get();
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                    (string)device.GetPropertyValue("DeviceID"),
+                    (string)device.GetPropertyValue("SerialNumber"),
+                    (string)device.GetPropertyValue("PNPDeviceID").ToString().Split('\\').Last(),
+                    (string)device.GetPropertyValue("Description")
+                    )); ;
+            }
+            return devices;
+        }
+
+        class USBDeviceInfo
+        {
+            public USBDeviceInfo(string deviceID, string serialNumber, string pnpDeviceID, string description)
+            {
+                this.DeviceID = deviceID;
+                this.SerialNumber = serialNumber;
+                this.PnpDeviceID = pnpDeviceID;
+                this.Description = description;
+            }
+
+            public string DeviceID { get; private set; }
+            public string SerialNumber { get; private set; }
+            public string PnpDeviceID { get; private set; }
+            public string Description { get; private set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //EjectDrive(tbNameDisk.Text);
+        }
+
+        /*
+        const uint GENERIC_READ = 0x80000000;
+        const uint GENERIC_WRITE = 0x40000000;
+        const int FILE_SHARE_READ = 0x1;
+        const int FILE_SHARE_WRITE = 0x2;
+        const int FSCTL_LOCK_VOLUME = 0x00090018;
+        const int FSCTL_DISMOUNT_VOLUME = 0x00090020;
+        const int IOCTL_STORAGE_EJECT_MEDIA = 0x2D4808;
+        const int IOCTL_STORAGE_MEDIA_REMOVAL = 0x002D4804;
+
+        void EjectDrive(string driveLetter)
+        {
+            string path = @"\\.\" + driveLetter + @":";
+            IntPtr handle = CreateFile(path, GENERIC_READ | GENERIC_WRITE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE, IntPtr.Zero, 0x3, 0, IntPtr.Zero);
+
+            if ((long)handle == -1)
+            {
+                textBox1.AppendText("Unable to open drive " + driveLetter);
+                textBox1.AppendText(Environment.NewLine);
+                return;
+            }
+
+            int dummy = 0;
+
+            DeviceIoControl(handle, IOCTL_STORAGE_EJECT_MEDIA, IntPtr.Zero, 0,
+                IntPtr.Zero, 0, ref dummy, IntPtr.Zero);
+
+            CloseHandle(handle);
+
+            textBox1.AppendText("OK to remove drive.");
+            textBox1.AppendText(Environment.NewLine);
+        }
+        [DllImport("kernel32", SetLastError = true)]
+        private static extern IntPtr CreateFile
+            (string filename, uint desiredAccess,
+                uint shareMode, IntPtr securityAttributes,
+                int creationDisposition, int flagsAndAttributes,
+                IntPtr templateFile);
+        [DllImport("kernel32")]
+        private static extern int DeviceIoControl
+            (IntPtr deviceHandle, uint ioControlCode,
+                IntPtr inBuffer, int inBufferSize,
+                IntPtr outBuffer, int outBufferSize,
+                ref int bytesReturned, IntPtr overlapped);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+        */
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eject(USBEject("G:"));
+        }
+    }
+}
